fec61418b4f69812607dae366ff55f9d7158780c
Anthony Finch <t@anthonyfinch.com>
2020-04-19 23:29:53 +0100

    adds total to money display

--- components/MoneyDisplay.gd
+++ components/MoneyDisplay.gd
@@ -6,11 +6,11 @@ var _money: int = 0
 
 func _ready():
 	._ready()
-	_parent_node.text = "Money: " + String(_money)
+	_parent_node.text = "Money: " + String(_money) + " / 1000"
 
 
 func on_state_changed(state: Dictionary)-> void:
 	var new_money = state["money"]
 	if new_money != _money:
 		_money = new_money
-		_parent_node.text = "Money: " + String(new_money)
+		_parent_node.text = "Money: " + String(new_money) + " / 1000"
