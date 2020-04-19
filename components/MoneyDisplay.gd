d10f079bb8e140c64a2f05060b00344769d0505e
Anthony Finch <t@anthonyfinch.com>
2020-04-20 00:12:35 +0100

    ten starting tattoos in

--- components/MoneyDisplay.gd
+++ components/MoneyDisplay.gd
@@ -6,11 +6,11 @@ var _money: int = 0
 
 func _ready():
 	._ready()
-	_parent_node.text = "Money: " + String(_money) + " / 1000"
+	_parent_node.text = "Money: " + String(_money) + " of 1000"
 
 
 func on_state_changed(state: Dictionary)-> void:
 	var new_money = state["money"]
 	if new_money != _money:
 		_money = new_money
-		_parent_node.text = "Money: " + String(new_money) + " / 1000"
+		_parent_node.text = "Money: " + String(new_money) + " of 1000"
